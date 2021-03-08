e8dc52f1c3177a30cea092af438ca840c9ce25f3
Alsania14 <alingotama14@gmail.com>
2021-03-08 20:56:27 +0800

    Update SplashScreenActivity.java

--- app/src/main/java/id/alin/espeedboat/SplashScreenActivity.java
+++ app/src/main/java/id/alin/espeedboat/SplashScreenActivity.java
@@ -36,7 +36,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             new Handler().postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    Intent intent = new Intent(SplashScreenActivity.this, RegisterActivity.class);
                     startActivity(intent);
                     finish();
                 }
