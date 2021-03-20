ca3e9da53ed5108c3ead7797111f71e8af43cc03
Alsania14 <alingotama14@gmail.com>
2021-03-20 12:01:03 +0800

    Update SplashScreenActivity.java

--- app/src/main/java/id/alin/espeedboat/SplashScreenActivity.java
+++ app/src/main/java/id/alin/espeedboat/SplashScreenActivity.java
@@ -21,6 +21,7 @@ public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
