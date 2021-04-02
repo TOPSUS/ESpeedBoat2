1e2c6c696b9e67e30ad31be7876596d16d8f414a
Alsania14 <alingotama14@gmail.com>
2021-04-03 00:46:49 +0800

    menambahkan upload bukti pembayaran
    bukti pembayaran sudah bisa, otw batalkan pembayaran

--- app/src/main/java/id/alin/espeedboat/MyRetrofit/Services/PembelianServices.java
+++ app/src/main/java/id/alin/espeedboat/MyRetrofit/Services/PembelianServices.java
@@ -1,13 +1,21 @@
 package id.alin.espeedboat.MyRetrofit.Services;
 
+import java.util.Map;
+
 import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.DetailPembelian.ServerResponseDetailPembelian;
 import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.Jadwal.ServerResponseJadwalData;
 import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.Pembelian.ServerResponsePembelian;
+import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.ServerResponseModels;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
+import retrofit2.http.HeaderMap;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface PembelianServices {
     @FormUrlEncoded
@@ -22,5 +30,13 @@ public interface PembelianServices {
     Call<ServerResponseDetailPembelian> getDetailPembelian(
             @Header("Authorization") String Authorization,
             @Field("id") long id
+    );
+
+    @Multipart
+    @POST("/api/postbuktipembayaran")
+    Call<ServerResponseModels> postBuktiPembayaran(
+            @HeaderMap Map<String, String> Header,
+            @Part MultipartBody.Part image_bukti_pembayaran,
+            @Part("id_pembelian") RequestBody id_pembelian
     );
 }
--- app/src/main/java/id/alin/espeedboat/MyUnpaidDetailTransactionActivity.java
+++ app/src/main/java/id/alin/espeedboat/MyUnpaidDetailTransactionActivity.java
@@ -1,35 +1,45 @@
 package id.alin.espeedboat;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.RecyclerView;
-import id.alin.espeedboat.MyAdapter.MyUnpaidTransactionAdapter;
+
+import dmax.dialog.SpotsDialog;
 import id.alin.espeedboat.MyAdapter.PenumpangDetailAdapter;
 import id.alin.espeedboat.MyRetrofit.ApiClient;
 import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.DetailPembelian.ServerResponseDetailPembelian;
-import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.Pembelian.ServerResponsePembelian;
+import id.alin.espeedboat.MyRetrofit.ServiceResponseModels.ServerResponseModels;
 import id.alin.espeedboat.MyRetrofit.Services.PembelianServices;
-import id.alin.espeedboat.MyRoom.Entity.PembelianEntitiy;
 import id.alin.espeedboat.MyRoom.Entity.PenumpangDetailEntity;
+import id.alin.espeedboat.MyViewModel.MainActivityViewModel.ObjectData.ProfileData;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -37,11 +47,16 @@ import android.widget.Toast;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.rajat.pdfviewer.PdfViewerActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.BinaryOperator;
+import java.util.Map;
 
 public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
     private long id = 0;
@@ -56,13 +71,19 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     PenumpangDetailAdapter penumpangDetailAdapter;
 
-    /*BOTTOM SHEET UPLOAD BUKTI*/
+    //*BOTTOM SHEET UPLOAD BUKTI*/
     private BottomSheetDialog bottomSheetDialog;
     private View bottomsheetview;
+    private File file_image_bukti;
+
+    //ATRIBUTE STATIC PROSES PEMBAYARAN
+    private static final String IMAGE_PEMBAYARAN = "IMAGE_PEMBAYARAN";
+    private static final String IMAGE_PEMBAYARAN_FILE_NAME = "IMAGE_PEMBAYARAN_FILE_NAME.jpg";
+    private Dialog loadingdialog;
 
     /*ATRIBUTE KAMERA UNTUK NGAMBIL BUKTI PEMBAYARAN*/
-    private static final int CAMERA_REQUEST = 1888;
     private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    private static final int CAMERA_REQUEST = 2;
 
     /*ATRIBUTE STORAGE UNTUK NGAMBIL BUKTI PEMBAYARAN*/
     private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
@@ -157,6 +178,8 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
                     setRecyclerView(response.body().getPenumpang());
                     setData(response.body());
                     setStateReady();
+                    setStateTransparentLoading(false);
+                    showBottomDialogUploadBukti(false);
                 }else{
                     setStateNodata();
                     Toast.makeText(MyUnpaidDetailTransactionActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
@@ -263,9 +286,42 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
     }
 
     //KELOLA PEMBAYARAB
-    //UPLOAD BUKTI PEMBAYARAN
-    private void postBuktiPembayaranToApi(){
 
+    //UPLOAD BUKTI PEMBAYARAN
+    private void postBuktiPembayaranToApi(File bukti_pembayaran, String token, String id_user){
+        //JALANKAN LOADING TRANSPARENT STATE
+        setStateTransparentLoading(true);
+
+        //MEMBUAT SEBUAH REQUEST BODY UNTUK MULTIPART
+        RequestBody requestFile = RequestBody.create(bukti_pembayaran,MediaType.parse("multipart/form-data"));
+        MultipartBody.Part body = MultipartBody.Part.createFormData("image_bukti_pembayaran",IMAGE_PEMBAYARAN, requestFile);
+
+        Map<String,String> headers = new HashMap<>();
+        headers.put("Authorization",MainActivity.mainActivityViewModel.getProfileLiveData().getValue().getToken());
+
+        RequestBody id_pembelian = RequestBody.create(String.valueOf(id),(MediaType.parse("multipart/form-data")));
+
+        //MEMANGGIL LIBRARY RETROFIT UNTUK POST BUKTI PEMBAYARAN
+        PembelianServices pembelianServices = ApiClient.getRetrofit().create(PembelianServices.class);
+        Call<ServerResponseModels> call = pembelianServices.postBuktiPembayaran(
+                headers,
+                body,
+                id_pembelian
+        );
+
+        call.enqueue(new Callback<ServerResponseModels>() {
+            @Override
+            public void onResponse(Call<ServerResponseModels> call, Response<ServerResponseModels> response) {
+                Toast.makeText(MyUnpaidDetailTransactionActivity.this, response.body().getMessage(), Toast.LENGTH_SHORT).show();
+                postDetailPesananFromApi();
+            }
+
+            @Override
+            public void onFailure(Call<ServerResponseModels> call, Throwable t) {
+                Toast.makeText(MyUnpaidDetailTransactionActivity.this, t.getMessage(), Toast.LENGTH_SHORT).show();
+                postDetailPesananFromApi();
+            }
+        });
     }
 
     //BATALKAN PESANAN
@@ -289,27 +345,40 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
         recyclerView.setAdapter(penumpangDetailAdapter);
     }
 
-    /*DIJALANKAN SAAT API BERHASIL MENGAMBIL DATA DARI SERVER DAN JUMLAHNYA LEBIH DARI 0*/
+    //DIJALANKAN SAAT API BERHASIL MENGAMBIL DATA DARI SERVER DAN JUMLAHNYA LEBIH DARI 0
     private void setStateReady(){
         this.loading.setVisibility(View.INVISIBLE);
         this.nodata.setVisibility(View.INVISIBLE);
         this.detail.setVisibility(View.VISIBLE);
     }
 
-    /*STATE LOADING SAAT APLIKASI SEDANG MEMINTA DATA PELABUHAN KE SERVER*/
+    //STATE LOADING SAAT APLIKASI SEDANG MEMINTA DATA PELABUHAN KE SERVER*/
     private void setStateLoading(){
         this.loading.setVisibility(View.VISIBLE);
         this.nodata.setVisibility(View.INVISIBLE);
         this.detail.setVisibility(View.INVISIBLE);
     }
 
-    /*STATE NO DATA SAAT DATA YANG DIBERIKAN OLEH SERVER KOSONG*/
+    //STATE NO DATA SAAT DATA YANG DIBERIKAN OLEH SERVER KOSONG*/
     private void setStateNodata(){
         this.loading.setVisibility(View.INVISIBLE);
         this.nodata.setVisibility(View.VISIBLE);
     }
 
-    /*COUNT DOWN METHOD*/
+    //SHOW LOADING STATE
+    private void setStateTransparentLoading(boolean status){
+        if(status){
+            this.loadingdialog = new SpotsDialog.Builder().setContext(this).setMessage("Mohon tunggu sebentar").build();
+            this.loadingdialog.show();
+        }else{
+            if(this.loadingdialog != null && this.loadingdialog instanceof Dialog){
+                this.loadingdialog.dismiss();
+            }
+            this.loadingdialog = null;
+        }
+    }
+
+    //*COUNT DOWN METHOD*/
     private void countDown(){
         countDownTimer = new CountDownTimer(timeLeft, 10){
             @Override
@@ -325,7 +394,7 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
         }.start();
     }
 
-    /*UPDATE METHOD UNTUK COUNTDOWN*/
+    //*UPDATE METHOD UNTUK COUNTDOWN*/
     private void updateTimer(){
         int menit = (int) timeLeft/60000;
         int detik = (int) timeLeft%60000/1000;
@@ -343,7 +412,7 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
         }
     }
 
-    /*SHOW BOTTOM DIALOG UNTUK UPLOPOAD BUKTI PEMBAYARAN*/
+    //METHOD UNTUK MEMPERLIHATKAN BOTTOMSHEETDIALOG
     private void showBottomDialogUploadBukti(boolean status){
         /*APABILA TRUE MAKA BUKA DIALOG*/
         if(status){
@@ -351,6 +420,7 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
             bottomsheetview = LayoutInflater.from(getApplicationContext()).inflate(R.layout.bottom_sheet_ambil_bukti_pembayaran, findViewById(R.id.bottom_sheet_ambil_bukti_pembayaran));
             bottomSheetDialog.setContentView(bottomsheetview);
 
+            //MENGATUR BTN CAMERA LIESTENER
             Button btn_camere = bottomsheetview.findViewById(R.id.btnMiniFragmentRegisterActivityTambahgambar);
             btn_camere.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -359,6 +429,7 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
                 }
             });
 
+            //MENGATUR BTN STORAGE LIESTENER
             Button btn_storage = bottomsheetview.findViewById(R.id.storage);
             btn_storage.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -367,18 +438,43 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
                 }
             });
 
+            //MENGATUR BTN KIRIM LIESTENER
+            Button btn_kirim = bottomsheetview.findViewById(R.id.buttonkirim);
+            btn_kirim.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(file_image_bukti.exists()){
+                        ProfileData profileData = MainActivity.mainActivityViewModel.getProfileLiveData().getValue();
+
+                        postBuktiPembayaranToApi(file_image_bukti,profileData.getToken(),profileData.getUser_id());
+
+                    }else{
+                        Toast.makeText(MyUnpaidDetailTransactionActivity.this, "TERJADI KESALAHAN", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    file_image_bukti = null;
+                }
+            });
+
             bottomSheetDialog.show();
         }else{
             if(this.bottomSheetDialog != null){
                 if(this.bottomSheetDialog.isShowing()){
                     this.bottomSheetDialog.dismiss();
+                    this.bottomSheetDialog = null;
                 }
             }
         }
     }
 
-    /*METHOD UNTUK MEMBUKA KAMERA UPLOAD BUKTI PEMBAYARAN*/
+    //METHOD UNTUK MEMBUKA KAMERA UPLOAD BUKTI PEMBAYARAN
     private void openCamera(){
+        /*CEK IZIN TERLEBIH DAHULU*/
         if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
         {
             requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
@@ -390,7 +486,7 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
         }
     }
 
-    /*METHOD UNTUK MEMBUKA STORAGE DAN MENGAMBIL BUKTI PEMBAYARAN*/
+    //METHOD UNTUK MEMBUKA STORAGE DAN MENGAMBIL BUKTI PEMBAYARAN*/
     private void pickFile() {
         if(requestRead()){
             Intent fileintent = new Intent(Intent.ACTION_PICK);
@@ -403,7 +499,7 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
         }
     }
 
-    /*REQUEST READ FILE*/
+    //REQUEST READ FILE*/
     public boolean requestRead() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -419,7 +515,79 @@ public class MyUnpaidDetailTransactionActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        final int SUCCES_RESULT_CODE = -1;
+
         super.onActivityResult(requestCode, resultCode, data);
-        showBottomDialogUploadBukti(false);
+
+        /*CAMERA*/
+        if(requestCode == CAMERA_REQUEST && resultCode == SUCCES_RESULT_CODE){
+            /*CHECK BOTTOM SHEET VIEW AKTIF ATAU TIDAK*/
+            if(this.bottomsheetview != null && this.bottomsheetview.isShown()){
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+                ImageView imageView = this.bottomsheetview.findViewById(R.id.invoicebuktiimage);
+                imageView.setImageBitmap(bitmap);
+
+                this.file_image_bukti = convertBitmapToFile(bitmap);
+            }else{
+                Toast.makeText(this, "TERJADI KESALAHAN", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        /*STORAGE*/
+        if(requestCode == GALLERY_ADD_PROFILE && resultCode == SUCCES_RESULT_CODE){
+            Uri imgUri = data.getData();
+
+            /*CHECK BOTTOM SHEET VIEW AKTIF ATAU TIDAK*/
+            if(this.bottomsheetview != null && this.bottomsheetview.isShown()){
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),imgUri);
+
+                    ImageView imageView = this.bottomsheetview.findViewById(R.id.invoicebuktiimage);
+                    imageView.setImageBitmap(bitmap);
+
+                    this.file_image_bukti = convertBitmapToFile(bitmap);
+
+                } catch (Exception e) {
+                    Toast.makeText(this, "TERJADI KESALAHAN", Toast.LENGTH_SHORT).show();
+                }
+            }else{
+                Toast.makeText(this, "TERJADI KESALAHAN", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
+
+    //BITMAP TO FILE CONVERTER METHOD
+    private File convertBitmapToFile(Bitmap bitmap){
+        //create a file to write bitmap data
+        File f = new File(getCacheDir(), IMAGE_PEMBAYARAN_FILE_NAME);
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Convert bitmap to byte array
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 20 /*ignored for PNG*/, bos);
+        byte[] bitmapdata = bos.toByteArray();
+
+        //write the bytes in file
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(f,false);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return f;
+    }
+
 }
